27a42764ef7ebaa9eea6cb44d32576ab05631e2f
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2023-09-14 20:06:25 -0300

    utilizando .env

--- config.php
+++ config.php
@@ -1,24 +1,18 @@
 <?php
-require "enviroment.php";
+require 'vendor/autoload.php';
 
-$data = array();
-if(ENVIROMENT == 'development'){
-    define('BASE_URL', 'http://localhost/bonita-flor/');
-    $data['host'] = 'srv1056.hstgr.io';
-    $data['db_name'] = 'u729110013_bnt_flor';
-    $data['user_db'] = 'u729110013_bnt_flor_sys';
-    $data['user_pass_db'] = 'ISbaP2r>';
-}else{
-    define('BASE_URL', 'https://www.admin.benurse.com.br/');
-    $data['host'] = 'localhost';
-    $data['db_name'] = 'benurs81_benurse';
-    $data['user_db'] = 'benurs81_admin';
-    $data['user_pass_db'] = 'Sacramento1@';
-}
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->loadEnv(__DIR__.'/.env');
+
+header("Access-Control-Allow-Origin: *");
+
+define('BASE_URL', $_ENV['BASE_URL']);
 
 global $db;
 try{
-    $db = new PDO('mysql:dbname='.$data['db_name'].';host='.$data['host'], $data['user_db'], $data['user_pass_db']);
+    $db = new PDO('mysql:dbname='.$_ENV['DB_NAME'].';host='.$_ENV['HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 }catch(PDOException $e){
     echo "Error: ".$e->getMessage();
     exit;
