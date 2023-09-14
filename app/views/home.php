992bff278c762090d2bda4e88a450351f2a061fc
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2023-09-13 23:11:59 -0300

    removendo sessões que não serão utilizadas

--- app/views/home.php
+++ app/views/home.php
@@ -43,7 +43,7 @@
                      <div class="full">
                         <div class="center-desk">
                            <div class="logo">
-                              <a href="index.html"><img src="<?=BASE_URL?>/app/assets/images/logo.png" alt="#" /></a>
+                              <h1>Bonita Flor</h1> 
                            </div>
                         </div>
                      </div>
@@ -63,12 +63,6 @@
                               </li>
                               <li class="nav-item">
                                  <a class="nav-link" href="we-do.html">What we do</a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link" href="pricing.html">Pricing</a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link" href="contact.html">Contact Us</a>
                               </li>
                            </ul>
                         </div>
@@ -90,72 +84,17 @@
       <!-- end header -->
       <!-- banner -->
       <section class="banner_main">
-         <div id="myCarousel" class="carousel slide banner1" data-ride="carousel">
-            <ol class="carousel-indicators">
-               <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-               <li data-target="#myCarousel" data-slide-to="1"></li>
-               <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-               <div class="carousel-item active">
-                  <div class="container-fluid">
-                     <div class="carousel-caption relative">
-                        <div class="row d_flex">
-                           <div class="col-md-6">
-                              <img class="bann_img" src="<?=BASE_URL?>/app/assets/images/banner_ing.jpg" alt="#"/>
-                           </div>
-                           <div class="col-md-6">
-                              <span>01/03</span>
-                              <h1>Milina nail Salon Creating Beauty</h1>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo</p>
-                              <a class="get_btn" href="Javascript:void(0)" role="button">Get Appoinment</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
+         <div class="container-fluid">
+            <div class="row d_flex relative slide banner1">
+               <div class="col-md-6">
+                  <img class="bann_img" src="<?=BASE_URL?>/app/assets/images/banner_ing.jpg" alt="#"/>
                </div>
-               <div class="carousel-item">
-                  <div class="container-fluid">
-                     <div class="carousel-caption relative">
-                        <div class="row d_flex">
-                           <div class="col-md-6">
-                              <img class="bann_img" src="<?=BASE_URL?>/app/assets/images/banner_ing.jpg" alt="#"/>
-                           </div>
-                           <div class="col-md-6">
-                              <span>02/03</span>
-                              <h1>Milina nail Salon Creating Beauty</h1>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo</p>
-                              <a class="get_btn" href="Javascript:void(0)" role="button">Get Appoinment</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
+               <div class="col-md-6">
+                  <span></span>
+                  <h1>Milina nail Salon Creating Beauty</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo</p>
+                  <a class="get_btn" href="Javascript:void(0)" role="button">Get Appoinment</a>
                </div>
-               <div class="carousel-item">
-                  <div class="container-fluid">
-                     <div class="carousel-caption relative">
-                        <div class="row d_flex">
-                           <div class="col-md-6">
-                              <img class="bann_img" src="<?=BASE_URL?>/app/assets/images/banner_ing.jpg" alt="#"/>
-                           </div>
-                           <div class="col-md-6">
-                              <span>03/03</span>
-                              <h1>Milina nail Salon Creating Beauty</h1>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo</p>
-                              <a class="get_btn" href="Javascript:void(0)" role="button">Get Appoinment</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-               <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
-               <span class="sr-only">Previous</span>
-               </a>
-               <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-               <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-               <span class="sr-only">Next</span>
-               </a>
             </div>
          </div>
       </section>
@@ -227,59 +166,9 @@
          </div>
       </div>
       <!-- end about -->
-      <!-- our pricing -->
-      <div  class="pricing slin">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="titlepage">
-                     <h2>Our Pricing</h2>
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-md-4">
-                  <div class="pricing_box">
-                     <div class="pricing_box_ti">
-                        <h3> <span>$</span>20<strong>/one Day</strong></h3>
-                     </div>
-                     <div class="our_pricing">
-                        <span>Regular</span>
-                        <p>Lorem ipsum dolor sit amet, iscing elit, sed do magna</p>
-                     </div>
-                  </div>
-                  <a class="read_more mar_top" href="Javascript:void(0)"> Book Now</a>
-               </div>
-               <div class="col-md-4">
-                  <div class="pricing_box">
-                     <div class="pricing_box_ti">
-                        <h3> <span>$</span>30<strong>/one Day</strong></h3>
-                     </div>
-                     <div class="our_pricing">
-                        <span>Standard</span>
-                        <p>Lorem ipsum dolor sit amet, iscing elit, sed do magna</p>
-                     </div>
-                  </div>
-                  <a class="read_more mar_top" href="Javascript:void(0)"> Book Now</a>
-               </div>
-               <div class="col-md-4">
-                  <div class="pricing_box">
-                     <div class="pricing_box_ti">
-                        <h3> <span>$</span>40<strong>/one Day</strong></h3>
-                     </div>
-                     <div class="our_pricing">
-                        <span>Premium</span>
-                        <p>Lorem ipsum dolor sit amet, iscing elit, sed do magna</p>
-                     </div>
-                  </div>
-                  <a class="read_more mar_top" href="Javascript:void(0)"> Book Now</a>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- end our pricing -->
+
       <!-- experts -->
-      <div  class="experts slin2">
+      <div  class="experts">
          <div class="container">
             <div class="row">
                <div class="col-md-12">
@@ -333,53 +222,7 @@
          </div>
       </div>
       <!-- end experts -->
-      <!--  contact -->
-      <div class="contact">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="titlepage">
-                     <h2>Get In Touch</h2>
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-md-6 offset-md-3">
-                  <form id="request" class="main_form">
-                     <div class="row">
-                        <div class="col-md-12 ">
-                           <input class="contactus" placeholder="Full Name" type="type" name="Full Name"> 
-                        </div>
-                        <div class="col-md-12">
-                           <input class="contactus" placeholder="Email " type="type" name="Email "> 
-                        </div>
-                        <div class="col-md-12">
-                           <input class="contactus" placeholder="Phone Number" type="type" name="Phone Number">                          
-                        </div>
-                        <div class="col-md-12">
-                           <textarea class="textarea" placeholder="Message" type="type" Message="Name">Message</textarea>
-                        </div>
-                        <div class="col-md-12">
-                           <button class="send_btn">Send</button>
-                        </div>
-                     </div>
-                  </form>
-               </div>
-            </div>
-         </div>
-         <div class="container-fluid">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="map_main">
-                     <div class="map-responsive">
-                        <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&amp;q=Eiffel+Tower+Paris+France" width="600" height="386" frameborder="0" style="border:0; width: 100%;" allowfullscreen=""></iframe>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- end contact -->
+      
       <!--  footer -->
       <footer>
          <div class="footer">
@@ -406,8 +249,6 @@
                         <li class="active"><a href="index.html">Home</a></li>
                         <li><a href="about.html"> About</a></li>
                         <li><a href="we-do.html">What we do</a></li>
-                        <li><a href="pricing.html">Pricing</a></li>
-                        <li><a href="contact.html">Contact Us</a></li>
                      </ul>
                   </div>
                   <div class="col-md-4 col-sm-6">
