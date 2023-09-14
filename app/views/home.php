c28b3a57b3278fb5cb229edc371d8443709424f3
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2023-09-13 23:17:47 -0300

    removendo footer e alterando copyright

--- app/views/home.php
+++ app/views/home.php
@@ -226,46 +226,11 @@
       <!--  footer -->
       <footer>
          <div class="footer">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-12">
-                     <ul class="conta">
-                        <li><i class="fa fa-map-marker" aria-hidden="true"></i> Passages of Lorem Ipsum available</li>
-                        <li><i class="fa fa-phone" aria-hidden="true"></i> Call : +012334567890</li>
-                        <li> <i class="fa fa-envelope" aria-hidden="true"></i><a href="#"> demo@gmail.com</a></li>
-                     </ul>
-                  </div>
-                  <div class=" col-md-3 col-sm-6">
-                     <h3>About </h3>
-                     <p class="variat">There are many variations of passages of Lorem Ipsum available, but the majority have suffered le</p>
-                  </div>
-                  <div class=" col-md-3 col-sm-6">
-                     <h3>Nail Salon  </h3>
-                     <p class="variat" >There are many variations of passages of Lorem Ipsum available, but the majority have suffered le</p>
-                  </div>
-                  <div class="col-md-2 col-sm-6">
-                     <h3>Useful Link</h3>
-                     <ul class="link_menu">
-                        <li class="active"><a href="index.html">Home</a></li>
-                        <li><a href="about.html"> About</a></li>
-                        <li><a href="we-do.html">What we do</a></li>
-                     </ul>
-                  </div>
-                  <div class="col-md-4 col-sm-6">
-                     <h3>Subscribe</h3>
-                     <form class="bottom_form">
-                        <a class="right_btn" href="Javascript:void(0)"> <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-                        <input class="enter" placeholder="Enter your email" type="text" name="Enter your email">
-                        <p >There are many variations of passages </p>
-                     </form>
-                  </div>
-               </div>
-            </div>
             <div class="copyright">
                <div class="container">
                   <div class="row">
                      <div class="col-md-10 offset-md-1">
-                        <p>© 2019 All Rights Reserved. Design by  <a href="https://html.design/"> Free Html Templates</a> Distributed By <a href="https://themewagon.com">ThemeWagon</a></p>
+                        <p>© 2023 All Rights Reserved. Design by  <a href="https://github.com/bz-software" target="_blank">BZ Software</p>
                      </div>
                   </div>
                </div>
